0130ae866348fb2009e3f750f0a494c9b072b1a1
nandavankalapati <nandavankalapati@gmail.com>
2026-06-07 11:44:16 +0530

    Update Medical Chatbot project

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
@@ -13,14 +12,12 @@ app = Flask(__name__)
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
-
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 embedding = download_embeddings()
 
@@ -32,17 +29,33 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity",search_kwargs={"k":3})
 
-chatModel = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3, google_api_key=GEMINI_API_KEY)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+llm = ChatGroq(
+                model="llama-3.3-70b-versatile",
+                temperature=0.0
+            )
 
-question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+prompt =ChatPromptTemplate.from_template("""
+You are a friendly medical assistant.
+
+Rules:
+1. If the user greets you (Hi, Hello, Good Morning, etc.), respond politely.
+2. If the answer is available in the provided context, answer using the context.
+3. If the answer is not available in the context, simply say:
+   "I don't know the answer to that question."
+4. Do not mention the context or say things like
+   "The information is not available in the context."
+
+Context:
+{context}
+
+Question:
+{input}
+""")
+
+chain = prompt | llm | StrOutputParser()
+
 
 @app.route("/")
 def index():
@@ -53,9 +66,16 @@ def chat():
     msg = request.form["msg"]
     input = msg
     print(input)
-    response = rag_chain.invoke({"input":msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
+    docs = retriever.invoke(msg)
+    context = "\n\n".join(
+        doc.page_content for doc in docs
+    )
+    response = chain.invoke({
+        "context": context,
+        "input": msg
+    })
+    print("Response:", response)
+    return response
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 8080, debug = True)
